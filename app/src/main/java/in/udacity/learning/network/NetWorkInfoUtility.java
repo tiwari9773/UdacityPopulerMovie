0ef94e1e5cd6db77b06138cd647973ea6f5919af
Lokesh Tiwari <tiwari9773@gmail.com>
2015-11-17 01:33:55 +0530

    Minor Network checking code change

--- app/src/main/java/in/udacity/learning/network/NetWorkInfoUtility.java
+++ app/src/main/java/in/udacity/learning/network/NetWorkInfoUtility.java
@@ -3,6 +3,7 @@ package in.udacity.learning.network;
 import android.app.Application;
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
 import android.util.Log;
 import android.widget.Toast;
@@ -39,38 +40,57 @@ class NetWorkInfoUtility {
     private boolean isWifiEnable = false;
     private boolean isMobileNetworkAvailable = false;
 
+
     public boolean isNetWorkAvailableNow(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        setIsWifiEnable(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected());
-        setIsMobileNetworkAvailable(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected());
-
-        if (isWifiEnable() || isMobileNetworkAvailable()) {
-            /*Sometime wifi is connected but service provider never connected to internet
-            so cross check one more time*/
-            if (isOnline(context))
-                isNetworkAvailable = true;
-        }
-
-        return isNetworkAvailable;
-    }
-
-    public boolean isOnline(Context context) {
-        long t = Calendar.getInstance().getTimeInMillis();
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            /*Pinging to Google server*/
-            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
-            int exitValue = ipProcess.waitFor();
-            return (exitValue == 0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            long t2 = Calendar.getInstance().getTimeInMillis();
-            Log.i("NetWork check Time", (t2 - t) + "");
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            /*
+            *It is deprecated so what is new way, I have searched on stackoverflow many people are taking help of
+            * ping on 8.8.8.8
+            */
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            //Network[] info = connectivity.getAllNetworks();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
         }
         return false;
     }
+    /*Old Way to check network Connection*/
+//    public boolean isNetWorkAvailableNow(Context context) {
+//        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+//
+//        setIsWifiEnable(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected());
+//        setIsMobileNetworkAvailable(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected());
+//
+//        if (isWifiEnable() || isMobileNetworkAvailable()) {
+//            /*Sometime wifi is connected but service provider never connected to internet
+//            so cross check one more time*/
+//            if (isOnline(context))
+//                isNetworkAvailable = true;
+//        }
+//
+//        return isNetworkAvailable;
+//    }
+//
+//    public boolean isOnline(Context context) {
+//        long t = Calendar.getInstance().getTimeInMillis();
+//        Runtime runtime = Runtime.getRuntime();
+//        try {
+//            /*Pinging to Google server*/
+//            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+//            int exitValue = ipProcess.waitFor();
+//            return (exitValue == 0);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } finally {
+//            long t2 = Calendar.getInstance().getTimeInMillis();
+//            Log.i("NetWork check Time", (t2 - t) + "");
+//        }
+//        return false;
+//    }
 }
